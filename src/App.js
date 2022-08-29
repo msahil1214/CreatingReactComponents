1d91ff5eadd90b4351e0b86178b62eef961d3c1f
Muhammad Sahil <59023207+msahil1214@users.noreply.github.com>
2022-08-29 15:47:06 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Heading from "./Heading";
+import List from "./List";
+export default function App() {
+  return (
+    <div className="App">
+      <Heading />
+      <List />
+      <List />
+    </div>
+  );
+}
